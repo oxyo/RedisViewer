d165469b39113c4c2bca50f78af9b142096350a3
David Slusky <dave@slusky.org>
2020-01-29 08:00:57 -0500

    Increased default window size.

--- src/org/xenotek/redisviewer/controller/RedisViewer.java
+++ src/org/xenotek/redisviewer/controller/RedisViewer.java
@@ -49,7 +49,7 @@ public class RedisViewer extends JFrame {
 
     public static void main(String[] args) {
         System.setProperty("awt.useSystemAAFontSettings","on");
-        new RedisViewer("RedisViewer", 600, 400);
+        new RedisViewer("RedisViewer", 700, 500);
     }
 
     private class BrowserHandler implements BrowserListener {
